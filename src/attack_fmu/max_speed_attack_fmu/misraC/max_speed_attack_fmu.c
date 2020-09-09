0642b95935350d64e818a2ffeca5065ae3ba7d57
Niccolò Scatena <speedjack95@gmail.com>
2020-09-09 10:58:28 +0200

    Remove unuseful if branch

--- src/attack_fmu/max_speed_attack_fmu/misraC/max_speed_attack_fmu.c
+++ src/attack_fmu/max_speed_attack_fmu/misraC/max_speed_attack_fmu.c
@@ -36,15 +36,7 @@ State* tick(State* st)
 	if (st->mode == X1 && st->cyclic && st->attack_duration > 0.0f && ((st->time + 0.00001f) > (st->attack_time + st->attack_duration)))
 		st->time = 0;
 
-	if (st->mode == X1 && ((st->time + 0.00001f) < st->attack_time)) {
-#ifdef DBG
-		_dbg_print_condition("st->mode == X1 && ((st->time + 0.00001f) < st->attack_time)");
-#endif
-		leave(X1, st);
-		st->output = st->input;
-		st->time = st->time + st->step_size;
-		enter(X1, st);
-	} else if (st->mode == X1 && ((st->time + 0.00001f) > st->attack_time) && ((st->attack_duration - 0.00001f) <= 0.0f || ((st->time - st->attack_time + 0.00001f) < st->attack_duration))) {
+	if (st->mode == X1 && ((st->time + 0.00001f) > st->attack_time) && ((st->attack_duration - 0.00001f) <= 0.0f || ((st->time - st->attack_time + 0.00001f) < st->attack_duration))) {
 #ifdef DBG
 		_dbg_print_condition("st->mode == X1 && ((st->time + 0.00001f) > st->attack_time) && ((st->attack_duration - 0.00001f) <= 0.0f || ((st->time - st->attack_time + 0.00001f) < st->attack_duration))");
 #endif
@@ -52,9 +44,7 @@ State* tick(State* st)
 		st->output = st->attack_value;
 		st->time = st->time + st->step_size;
 		enter(X1, st);
-	}
-
-	else if (st->mode == X1) {
+	} else if (st->mode == X1) {
 #ifdef DBG
 		_dbg_print_condition("st->mode == X1");
 #endif
