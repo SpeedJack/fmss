c40606350c5ae946bdc43180ededbbdb36fb78f8
Niccolò Scatena <speedjack95@gmail.com>
2020-09-11 15:42:40 +0200

    Add Sensors Output Attack doc

--- doc/listings/sensors-output-attack.c
+++ doc/listings/sensors-output-attack.c
@@ -1,34 +1,3 @@
-#include "sensor_output_attack_fmu.h"
-
-void init(State* st)
-{
-	st->previous_mode = X1;
-	st->mode = X1;
-	st->time = 0.0f;
-	st->step_size = 0.1f;
-	st->cyclic = false;
-	st->attack_time = 5.0f;
-	st->attack_duration = 5.0f;
-	st->attack_value = 0.5f;
-	st->input = 0.0f;
-	st->output = 0.0f;
-}
-
-void enter(Mode m, State* st)
-{
-	st->mode = m;
-}
-
-void leave(Mode m, State* st)
-{
-	st->previous_mode = m;
-}
-
-bool per_tick(State* st)
-{
-	return st->mode == X1;
-}
-
 State* tick(State* st)
 {
 	assert(per_tick(st));
@@ -42,9 +11,6 @@ State* tick(State* st)
 			&& ((st->time + 0.00001f) > st->attack_time)
 			&& ((st->attack_duration - 0.00001f) <= 0.0f
 				|| ((st->time - st->attack_time + 0.00001f) < st->attack_duration))) {
-#ifdef DBG
-		_dbg_print_condition("st->mode == X1 && ((st->time + 0.00001f) > st->attack_time) && ((st->attack_duration - 0.00001f) <= 0.0f || ((st->time - st->attack_time + 0.00001f) < st->attack_duration))");
-#endif
 		leave(X1, st);
 		st->output = st->input + st->input * st->attack_value;
 		if (st->output > 255.0f)
@@ -52,17 +18,11 @@ State* tick(State* st)
 		st->time = st->time + st->step_size;
 		enter(X1, st);
 	} else if (st->mode == X1) { /* do nothing: forward input */
-#ifdef DBG
-		_dbg_print_condition("st->mode == X1");
-#endif
 		leave(X1, st);
 		st->output = st->input;
 		st->time = st->time + st->step_size;
 		enter(X1, st);
 	}
 
-#ifdef DBG
-	_dbg_print_state(st);
-#endif
 	return st;
 }
