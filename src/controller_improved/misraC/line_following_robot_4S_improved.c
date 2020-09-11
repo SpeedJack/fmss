e99c67afe3301b05b6cbc791650fc308f4c1b448
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-09-11 10:56:24 +0200

    make fmu done

--- src/controller_improved/misraC/line_following_robot_4S_improved.c
+++ src/controller_improved/misraC/line_following_robot_4S_improved.c
@@ -1,8 +1,8 @@
 #include "line_following_robot_4S_improved.h"
 
 #define LSR_THRESHOLD 150.0f /* 0 = black; 255 = white; line color = white */
-#define MID_WEIGHT 2.0f; /*Used for weighted average*/
-#define FAR_WEIGHT 1.0f;
+#define MID_WEIGHT 2.0f /*Used for weighted average*/
+#define FAR_WEIGHT 1.0f
 
 void init(State* st)
 {
@@ -36,8 +36,8 @@ State* tick(State* st)
 	float64_t lfLeftVal = 0.0f;
 	if(st->mode == AUTO) {
 		/* compute sensor mean with weights */
-		lfRightVal = (st->lfFarRightVal * FAR_WEIGHT + st->lfMidRightVal * MID_WEIGHT) / (MID_WEIGHT + FAR_WERIGHT);
-		lfLeftVal = (st->lfFarLeftVal * FAR_WEIGHT + st->lfMidLeftVal * MID_WEIGHT) /  (MID_WEIGHT + FAR_WERIGHT);
+		lfRightVal = (st->lfFarRightVal * FAR_WEIGHT + st->lfMidRightVal * MID_WEIGHT) / (MID_WEIGHT + FAR_WEIGHT);
+		lfLeftVal = (st->lfFarLeftVal * FAR_WEIGHT + st->lfMidLeftVal * MID_WEIGHT) /  (MID_WEIGHT + FAR_WEIGHT);
 		
 		if (lfRightVal <= LSR_THRESHOLD && lfLeftVal <= LSR_THRESHOLD) { /* left black, right black -> forward */
 #ifdef DBG
